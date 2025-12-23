6d40ec52032cf85a19c95a6b4ebd29259d6840f4
asim <asimsaeed353@gmail.com>
2025-12-23 16:53:46 +0500

    Mutex

---
+++ multi-threading/Mutex/Mutex/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+
+namespace MutexDemo
+{
+    class Program
+    {
+        private static Mutex mutex = new Mutex();
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                Thread threadObj = new Thread(MutexDemo)
+                {
+                    Name = "Thread " + i
+                };
+
+                threadObj.Start();
+                Console.ReadKey();
+            }
+        }
+
+        static void MutexDemo ()
+        {
+            Console.WriteLine(Thread.CurrentThread.Name + " Wants to Enter Critical Section");
+            if(mutex.WaitOne(1000))
+            {
+                try
+                {
+                    Console.WriteLine("Success: " + Thread.CurrentThread.Name + " is Processing now");
+
+                    Thread.Sleep(2000);
+
+                    Console.WriteLine("Exit: " + Thread.CurrentThread.Name + " has completed its Tasks");
+                } finally
+                {
+                    mutex.ReleaseMutex();
+                    Console.WriteLine(Thread.CurrentThread.Name + " has released the Mutex");
+                }
+            }
+            else
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + " will not acquire mutex");
+            } 
+        }
+
+        ~Program() {
+            mutex.Dispose();
+        }
+    }
+}
---
+++ multi-threading/SingleInstanceMutex/SingleInstanceMutex/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace MutexDemo
+{
+    class Program
+    {
+        static Mutex _mutex;
+        static void Main(string[] args)
+        {
+            if(!IsSingleInstace())
+            {
+                Console.WriteLine("More Than One Instance");
+            } else
+            {
+                Console.WriteLine("Only One Instance");
+            }
+
+            Console.ReadKey();
+        }
+
+        static bool IsSingleInstace ()
+        {
+            try
+            {
+                // try opening existing instance 
+                // if there is no existing instance, then it will throw an exception
+                Mutex.OpenExisting("MyMutex");
+            }
+            catch
+            {
+                // make new instance
+                _mutex = new Mutex(true, "MyMutex");
+
+                return true;
+            }
+
+            // More than one instance
+            return false;
+        }
+    }
+}
